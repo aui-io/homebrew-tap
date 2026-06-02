b8c5c746d92cc0c0dc88639c118a184c9bcf5f7e
alaam-aui <alaam@aui.io>
2026-06-02 21:49:19 +0300

    Update aui.rb

--- Formula/aui.rb
+++ Formula/aui.rb
@@ -1,7 +1,7 @@
 class Aui < Formula
   desc "CLI for building, managing, and deploying AUI AI agent configurations"
   homepage "https://github.com/aui-io/aui-cli"
-  url "https://registry.npmjs.org/aui-agent-builder/-/aui-agent-builder-0.3.68.tgz"
+  url "https://registry.npmjs.org/aui-agent-builder/-/aui-agent-builder-0.3.115.tgz"
   license "SEE LICENSE IN LICENSE"
 
   depends_on "node"
